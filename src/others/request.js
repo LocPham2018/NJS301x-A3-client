5d86dece87f8f4afedd1baf8995457d1c9ad1fc4
LocPham2018 <38948787+LocPham2018@users.noreply.github.com>
2023-04-08 16:33:14 +0700

    change API domain

--- src/others/request.js
+++ src/others/request.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = 'http://localhost:5000';
+// export const SERVER_URL = 'http://localhost:5000'; // development purpose
+export const SERVER_URL = 'https://njs-301x-a3-server.vercel.app';
